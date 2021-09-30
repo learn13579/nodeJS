27e6ec22fecc1e1356ad691fbb3b256d47cd87c7
learn13579 <maryana.poshta@gmail.com>
2021-09-30 19:30:13 +0300

    lesson 1 (Victor)

---
+++ app.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+
+console.log(__dirname);
+console.log(__filename);
+
+fs.mkdir (path.join(__dirname,'men'), {recursive: true}, (err)=>{
+    if (err) {
+        console.log(err);
+    }
+} );
+
+fs.mkdir (path.join(__dirname,'women'), {recursive: true}, (err)=>{
+    if (err) {
+        console.log(err);
+    }
+} );
+
+const users = [
+    {name: 'Olya',   gender: 'female', age: 29},
+    {name: 'Tonya',  gender: 'female', age: 19},
+    {name: 'Solya',  gender: 'female', age: 23},
+    {name: 'Kolya',  gender: 'male',   age: 18},
+    {name: 'Ivan',   gender: 'male',   age: 33},
+    {name: 'Jenya',  gender: 'male',   age: 45},
+    {name: 'Max',    gender: 'female', age: 17},
+    {name: 'Nadya',  gender: 'female', age: 12},
+    {name: 'Oleg',   gender: 'male',   age: 37},
+    {name: 'Mariya', gender: 'female', age: 28},
+];
+
+const men = path.join(__dirname, 'users', 'men');
+const women = path.join(__dirname, 'users', 'women');
+
+for (const user of users) {
+    if (user.gender==="male"){
+        fs.writeFile(path.join(__dirname,'men',`${user.name}.txt`), `${user.name}, ${user.gender} ${user.age}`, (err)=>{
+            if (err){
+                console.log(err);
+            }
+        })
+    }
+     if (user.gender==="female"){
+        fs.writeFile(path.join(__dirname,'women',`${user.name}.txt`), `${user.name}, ${user.gender} ${user.age}`, (err)=>{
+            if (err){
+                console.log(err);
+            }
+        })
+    }
+}
