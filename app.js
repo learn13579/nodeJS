afb74fa97bf29a4b9ba8624375a8565dfe7e9638
learn13579 <maryana.poshta@gmail.com>
2021-10-01 17:18:26 +0300

    renewed lesson 1 part2

--- app.js
+++ app.js
@@ -1,61 +1,34 @@
 const fs = require('fs');
 const path = require('path');
 
-const men = path.join(__dirname, 'users', 'men');
-const women = path.join(__dirname, 'users', 'women');
+const pathMen = path.join(__dirname, 'men');
+const pathWomen = path.join(__dirname, 'women');
 
-console.log(__dirname);
-
-fs.mkdir(path.join(__dirname, 'men'), {recursive: true}, (err) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    fs.mkdir(path.join(__dirname, 'women'), {recursive: true}, (err) => {
+const sortGenders = (pathToDir, gender, newPathToDir) => {
+    fs.readdir(pathToDir, (err, files) => {
         if (err) {
             console.log(err);
+            return;
         }
-        sortMen(men);
-        sortWomen(women);
+
+        files.forEach(file => {
+            fs.readFile(path.join(pathToDir, file), (err, data) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+
+                let genderGo = JSON.parse(data.toString()).gender;
+
+                if (genderGo === gender) {
+                    fs.rename(path.join(pathToDir, file), path.join(newPathToDir, file), (err) => {
+                        console.log(err);
+                    });
+                }
+            });
+        });
     });
-});
-
-const sortGenders = (pathToDir) => {
-  fs.readdir (pathToDir, (err, files)  => {
-      if (err) {
-          console.log(err);
-          return;
-      }
-      fs.readFile(pathToDir, (err, files) => {
-          if (err) {
-              console.log(err);
-          }
-      })
-
-      let genderGo = data.toString();
-
-      if (genderGo.gender === male) {
-          fs.rename(path.join(pathToDir, file), path.join(newPathToDir, file), (err) => {
-              console.log(err);
-          })
-      }
-
-  })
-
 }
 
-// for (const user of users) {
-//     if (user.gender === "male") {
-//         fs.writeFile(path.join(__dirname, 'men', `${user.name}.txt`), `${user.name}, ${user.gender} ${user.age}`, (err) => {
-//             if (err) {
-//                 console.log(err);
-//             }
-//         })
-//     } else if (user.gender === "female") {
-//         fs.writeFile(path.join(__dirname, 'women', `${user.name}.txt`), `${user.name}, ${user.gender} ${user.age}`, (err) => {
-//             if (err) {
-//                 console.log(err);
-//             }
-//         })
-//     }
-// }
+sortGenders('women', "male", pathMen);
+sortGenders('men', "female", pathWomen);
