544c45c4661ffb484c1cb38a70926060be1bb56e
learn13579 <maryana.poshta@gmail.com>
2021-10-11 12:35:54 +0300

    com

--- router/user_router.js
+++ router/user_router.js
@@ -4,7 +4,7 @@ const userController = require('../controllers/user.controllers');
 const userMiddleware = require('../middlewares/edit.user.middleware');
 
 router.get('/', userController.getUsers);
-router.post('/', userMiddleware.createUserMiddleware, userController.createUser);
+router.post('/', userMiddleware.userEdit, userController.createUser);
 
 router.get('/:user_id', userController.getUserById);
 router.delete('/:userId', userMiddleware.userEdit, userController.deleteUser);
