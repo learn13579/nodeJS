a5faec9068399f3012530dff2c9949fc565fb836
learn13579 <maryana.poshta@gmail.com>
2021-10-11 12:33:26 +0300

    commit

--- controllers/authorization.controller.js
+++ controllers/authorization.controller.js
@@ -3,6 +3,7 @@ const User = require('../dataBase/User');
 module.exports = {
     authorizationUser: async (req, res) => {
         try {
+            const {email} = req.body.email;
             const loginUser = await User.findOne({email});
 
             res.json(loginUser);
