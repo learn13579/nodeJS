a7ff2ae7807baf6712cb5ee214efee5c2258e5c2
learn13579 <maryana.poshta@gmail.com>
2021-10-21 13:00:59 +0300

    commit

--- router/user_router.js
+++ router/user_router.js
@@ -10,10 +10,10 @@ const {
 const {userValidator, updateValidator} = require('../validators');
 
 router.get('/', getUsers);
-router.post('/', validMiddleware(userValidator), userEmailMiddleware, createUser);
+router.post('/', validMiddleware.isValidMiddleware(userValidator), userEmailMiddleware, createUser);
 
 router.get('/:user_id', userIdMiddleware, getUserById);
-router.put('/:user_id', validMiddleware(updateValidator), checkAccessToken, userIdMiddleware, updateUser);
+router.put('/:user_id', validMiddleware.isValidMiddleware(updateValidator), checkAccessToken, userIdMiddleware, updateUser);
 router.delete('/:user_id', checkAccessToken, userIdMiddleware, deleteAccount);
 
 module.exports = router;
