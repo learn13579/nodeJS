68b7f9417e4a6136e48a650808d3f3f36f99efd9
feden2906 <feden2906@gmail.com>
2021-10-21 12:26:12 +0300

    fix

--- router/user_router.js
+++ router/user_router.js
@@ -3,17 +3,18 @@ const router = require('express')
 
 const {userControllers: {getUsers, getUserById, createUser, updateUser, deleteAccount}} = require('../controllers');
 const {
-    validMiddleware,
+
+    validMiddleware: {ValidMiddleware},
     authMiddleware: {checkAccessToken},
     userMiddleware: {userIdMiddleware, userEmailMiddleware}
 } = require('../middlewares');
 const {userValidator, updateValidator} = require('../validators');
 
 router.get('/', getUsers);
-router.post('/', validMiddleware(userValidator), userEmailMiddleware, createUser);
+router.post('/', ValidMiddleware(userValidator), userEmailMiddleware, createUser);
 
 router.get('/:user_id', userIdMiddleware, getUserById);
-router.put('/:user_id', validMiddleware(updateValidator), checkAccessToken, userIdMiddleware, updateUser);
+router.put('/:user_id', ValidMiddleware(updateValidator), checkAccessToken, userIdMiddleware, updateUser);
 router.delete('/:user_id', checkAccessToken, userIdMiddleware, deleteAccount);
 
 module.exports = router;
